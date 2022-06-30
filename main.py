a38fae0b43561c00ae390b2136129632b8ddd9ca
unknown <sanwal@truefoundry.com>
2022-06-30 11:31:44 +0530

    Updating Version

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import yml
+
+
+def set_action_output(name: str, value: str):
+    sys.stdout.write(f'::set-output name={name}::{value}\n')
+
+
+def main():
+    input_file = os.environ["INPUT_FILE"]
+
+    with open(input_file, 'r') as file:
+        data = yaml.load(file)
+
+    x = data['version']
+    print(x)
+    x=x+1
+    print(x)
+
+    set_action_output('new-version', x)
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
